f3d56b79159cb7dd47f55d62bc74ea8271921893
Sean Elwood <seanelwood@gmail.com>
2017-11-06 17:48:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 let currentUser = 'Sean Elwood';
-let welcomeMessage = `Welcome to Flatbook,  + ${currentUser} + !`;
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, + ${currentUser.toUpperCase} + !`;
 
