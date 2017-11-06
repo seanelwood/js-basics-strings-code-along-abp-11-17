6adc089cb6057e8013e7deac6069840dac0801e5
Sean Elwood <seanelwood@gmail.com>
2017-11-06 17:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
-let shortGreeting = "Welcome, " + currentUser.push + "!";
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
