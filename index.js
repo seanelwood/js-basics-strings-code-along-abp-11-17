051787b06fab72abab7e1a50b1a117b3247d2284
Sean Elwood <seanelwood@gmail.com>
2017-11-06 17:58:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Sean Elwood';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
